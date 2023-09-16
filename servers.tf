f375e6a70442f2a734585f855de2fe473da00b8b
neeliveti <neeliveti.kirankumar@gmail.com>
2023-09-16 12:20:03 +0530

    terraform

--- servers.tf
+++ servers.tf
@@ -6,7 +6,13 @@ resource "aws_instance" "frontend" {
     Name = "frontend"
   }
 }
-
+resource "aws_route53_record" "www" {
+  zone_id = Z02149642661K5V8NCK02
+  name    = "frontend.kirandevops.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.frontend.private_ip]
+}
 output "frontend" {
 
    value = aws_instance.frontend.public_ip
@@ -21,6 +27,18 @@ resource "aws_instance" "redis" {
     Name = "redis"
   }
 }
+resource "aws_route53_record" "www" {
+  zone_id = Z02149642661K5V8NCK02
+  name    = "redis.kirandevops.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.redis.private_ip]
+}
+output "redis" {
+
+   value = aws_instance.redis.public_ip
+
+}
 resource "aws_instance" "mongodb" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t3.micro"
@@ -28,6 +46,19 @@ resource "aws_instance" "mongodb" {
   tags = {
     Name = "mongodb"
   }
+}
+
+resource "aws_route53_record" "www" {
+  zone_id = Z02149642661K5V8NCK02
+  name    = "mongodb.kirandevops.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.mongodb.private_ip]
+}
+output "mongodb" {
+
+   value = aws_instance.mongodb.public_ip
+
 }
 resource "aws_instance" "catalogue" {
   ami           = "ami-03265a0778a880afb"
@@ -37,6 +68,18 @@ resource "aws_instance" "catalogue" {
     Name = "catalogue"
   }
 }
+resource "aws_route53_record" "www" {
+  zone_id = Z02149642661K5V8NCK02
+  name    = "catalogue.kirandevops.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.catalogue.private_ip]
+}
+output "catalogue" {
+
+   value = aws_instance.catalogue.public_ip
+
+}
 resource "aws_instance" "cart" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t3.micro"
@@ -44,6 +87,18 @@ resource "aws_instance" "cart" {
   tags = {
     Name = "cart"
   }
+}
+resource "aws_route53_record" "www" {
+  zone_id = Z02149642661K5V8NCK02
+  name    = "cart.kirandevops.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.cart.private_ip]
+}
+output "cart" {
+
+   value = aws_instance.cart.public_ip
+
 }
 resource "aws_instance" "user" {
   ami           = "ami-03265a0778a880afb"
@@ -53,6 +108,18 @@ resource "aws_instance" "user" {
     Name = "user"
   }
 }
+resource "aws_route53_record" "www" {
+  zone_id = Z02149642661K5V8NCK02
+  name    = "user.kirandevops.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.user.private_ip]
+}
+output "user" {
+
+   value = aws_instance.user.public_ip
+
+}
 resource "aws_instance" "mysql" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t3.micro"
@@ -60,6 +127,18 @@ resource "aws_instance" "mysql" {
   tags = {
     Name = "mysql"
   }
+}
+resource "aws_route53_record" "www" {
+  zone_id = Z02149642661K5V8NCK02
+  name    = "mysql.kirandevops.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.mysql.private_ip]
+}
+output "mysql" {
+
+   value = aws_instance.mysql.public_ip
+
 }
 resource "aws_instance" "shipping" {
   ami           = "ami-03265a0778a880afb"
@@ -69,6 +148,18 @@ resource "aws_instance" "shipping" {
     Name = "shipping"
   }
 }
+resource "aws_route53_record" "www" {
+  zone_id = Z02149642661K5V8NCK02
+  name    = "shipping.kirandevops.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.shipping.private_ip]
+}
+output "shipping" {
+
+   value = aws_instance.shipping.public_ip
+
+}
 resource "aws_instance" "rabbitmq" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t3.micro"
@@ -76,6 +167,18 @@ resource "aws_instance" "rabbitmq" {
   tags = {
     Name = "rabbitmq"
   }
+}
+resource "aws_route53_record" "www" {
+  zone_id = Z02149642661K5V8NCK02
+  name    = "rabbitmq.kirandevops.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.rabbitmq.private_ip]
+}
+output "rabbitmq" {
+
+   value = aws_instance.rabbitmq.public_ip
+
 }
 resource "aws_instance" "payment" {
   ami           = "ami-03265a0778a880afb"
@@ -85,4 +188,15 @@ resource "aws_instance" "payment" {
     Name = "payment"
   }
 }
+resource "aws_route53_record" "www" {
+  zone_id = Z02149642661K5V8NCK02
+  name    = "payment.kirandevops.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.payment.private_ip]
+}
+output "payment" {
 
+   value = aws_instance.payment.public_ip
+
+}
